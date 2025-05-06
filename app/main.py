eb9934d82f00dd2ae6644c7f0188aa36c082fb32
andrewarlt <andrew.arlt@gmail.com>
2025-05-06 11:23:48 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -48,7 +48,7 @@ def database_to_geojson(table_name, geom_column='geometry'):
     conn.close()
 
     # Returning the data
-    return data
+    return data[0]
 
 # Create data route
 @app.route("/bmsb", methods=["GET"])
